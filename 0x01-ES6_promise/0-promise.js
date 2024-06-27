e5f7d02bfc2126dbd4abe75fa0ac6f679b0479f2
Emogy-reunion <mv7786986@gmail.com>
2024-06-27 05:30:29 +0300

    fix: replace code

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,3 @@
-export default function getFullResponseFromAPI(success) {
-  const array = new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
-  return array;
-};
+export default function getResponseFromAPI() {
+  return (new Promise((resolve, reject) => {}));
+}
--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,10 @@
-import getFullResponseFromAPI from './1-promise';
-
-console.log(getFullResponseFromAPI(true));
-console.log(getFullResponseFromAPI(false));
+export default function getFullResponseFromAPI(success) {
+  const array = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  return array;
+};
