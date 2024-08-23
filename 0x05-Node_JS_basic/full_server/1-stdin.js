124daa4c611e3a6d7292dcda6efa29f7b1edbee5
Emogy-reunion <mv7786986@gmail.com>
2024-08-23 16:36:41 +0300

    first commit

--- 0x05-Node_JS_basic/full_server/1-stdin.js
+++ 0x05-Node_JS_basic/full_server/1-stdin.js
@@ -1,9 +1,16 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("data", (input) => {
-	const name = input.toString().trim();
-	console.log(`Your name is: ${name}`);
-
-	process.stdout.write("This important software is now closing\n");
-	process.exit();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
