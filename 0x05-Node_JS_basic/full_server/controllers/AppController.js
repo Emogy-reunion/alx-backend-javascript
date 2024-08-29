737fc103f5354641c3edee94cc2a79c74ebfc8b6
Emogy-reunion <mv7786986@gmail.com>
2024-08-29 11:07:56 +0300

    first commit

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,9 @@
+// returns a 200 status and the message Hello Holberton School!
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
