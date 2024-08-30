167d1b8f7469f92739b67af15a2b74e9bb6f9016
Emogy-reunion <mv7786986@gmail.com>
2024-08-30 21:04:45 +0300

    first commit

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,12 @@
 export default function getFullResponseFromAPI(success) {
-  const array = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (success) {
-      resolve({ status: 200, body: 'Success' });
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
   });
-  return array;
-};
+}
---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let result = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    result = { photo, user };
+  } catch (err) {
+    result = { photo: null, user: null };
+  }
+  return result;
+}
